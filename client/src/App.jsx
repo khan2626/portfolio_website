9b4109d4eb1e090f3fa5ead06f13d3a2094be390
khan2626 <israelkhan217@gmail.com>
2026-03-11 17:16:34 +0100

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,58 +1,62 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import axios from 'axios'
+import axios from "axios";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Header from './components/Header'
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sider from "./components/Sider";
 import Loader from "./components/Loader";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { ShowLoading, HideLoading, SetPortfolioData } from "./redux/rootSlice.js";
-
-
+import {
+  ShowLoading,
+  HideLoading,
+  SetPortfolioData,
+} from "./redux/rootSlice.js";
 
 function App() {
-  
-  const { loading, portfolioData } = useSelector((state) => state.root)
-  const dispatch = useDispatch()
-  
+  const { loading, portfolioData } = useSelector((state) => state.root);
+  const dispatch = useDispatch();
+
   const getPortfolioData = async () => {
     try {
       dispatch(ShowLoading());
-      const apiUrl = process.env.NODE_ENV === 'development' ? '/portfolio_data' : 'https://khan-portfolio-website.onrender.com/portfolio_data';
+      const apiUrl =
+        "https://khan-portfolio-website.onrender.com/portfolio_data";
+      //const apiUrl = process.env.NODE_ENV === 'development' ? '/portfolio_data' : 'https://khan-portfolio-website.onrender.com/portfolio_data';
       const response = await axios.get(apiUrl);
       dispatch(SetPortfolioData(response.data));
     } catch (error) {
       console.error(error);
       // Fallback to local data if API fails
-      const { intro } = await import('./resources/intro_data');
-      const { abouts } = await import('./resources/intro_data');
-      const { projects } = await import('./resources/projects_data');
-      dispatch(SetPortfolioData({ intro: intro[0], about: abouts[0], projects }));
+      const { intro } = await import("./resources/intro_data");
+      const { abouts } = await import("./resources/intro_data");
+      const { projects } = await import("./resources/projects_data");
+      dispatch(
+        SetPortfolioData({ intro: intro[0], about: abouts[0], projects })
+      );
     } finally {
       dispatch(HideLoading());
     }
   };
 
-
-  useEffect(()=>{
-    if(!portfolioData){
-      getPortfolioData()
+  useEffect(() => {
+    if (!portfolioData) {
+      getPortfolioData();
     }
-  },[portfolioData])
+  }, [portfolioData]);
 
   return (
     <div className="bg-primary min-h-screen text-white">
       <BrowserRouter>
-      { loading && <Loader/>}
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-      </Routes>
-      <Footer/>
-      <Sider/>
+        {loading && <Loader />}
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+        <Sider />
       </BrowserRouter>
     </div>
   );
