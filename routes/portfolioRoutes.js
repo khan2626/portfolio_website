96a48292df706e7c57b1030a66f642b9b26d11a6
khan2626 <israelkhan217@gmail.com>
2026-06-04 15:35:10 +0100

    updated porfolioRoutes

--- routes/portfolioRoutes.js
+++ routes/portfolioRoutes.js
@@ -1,20 +1,20 @@
-const router = require('express').Router()
-const { Intro, About, Project } = require('../models/portfolioModels')
+const router = require("express").Router();
+const { Intro, About, Project } = require("../models/portfolioModels");
 
-router.get('/', async(req, res)=>{
-    try{
-        const intros = await Intro.find()
-        const abouts = await About.find()
-        const projects = await Project.find({})
-        res.status(200).json({
-            intro: intros[0],
-            about: abouts[0],
-            projects: projects || {}
-        })
-    } catch(error){
-        res.status(500).send(error)
-        console.log(error.message)
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const intros = await Intro.find();
+    const abouts = await About.find();
+    const projects = await Project.find({}).sort({ _id: -1 });
+    res.status(200).json({
+      intro: intros[0],
+      about: abouts[0],
+      projects: projects || {},
+    });
+  } catch (error) {
+    res.status(500).send(error);
+    console.log(error.message);
+  }
+});
 
 module.exports = router;
